52b96df3a5716c93a745e1760e3434bed8393a3d
tongz fungjai <srjerryz@gmail.com>
2018-03-23 13:30:14 +0700

    update path

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 app.post('/webhook', (req, res) => res.sendStatus(200))
+app.get('/webhook', (req, res) => res.sendStatus(200))
 app.listen(port)
