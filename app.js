ed3085ff8f746a11268075921f7680028aaa2956
tongz fungjai <srjerryz@gmail.com>
2018-03-23 13:55:46 +0700

    add reply

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.post('/webhook', (req, res) => {
     
 })
 app.listen(port)
-function reply(reply_token, body) {
+function reply(reply_token, user) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer 7NOT+yYEOQQM2QMFxwG+4Jg+RAA0iKiqPFG/BlKXTHUug5+xjcrlg2uDayjzNZe0RrrmIpHct0XiSgZp4o2G8DM8B1I+Ih5gHdPd/tgd519YQc0B5+gnAHiP6D4ZNEJ0LLhMybZl++xkNDBRbLg6yQdB04t89/1O/w1cDnyilFU='
@@ -33,7 +33,7 @@ function reply(reply_token, body) {
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: `ควยไร ${body.displayName}`
+            text: `ควยไร ${user.displayName}`
         }]
     })
     request.post({
