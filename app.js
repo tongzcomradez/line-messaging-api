c4c8e986a941a859122a3956bdae6c8ac5369388
tongz fungjai <srjerryz@gmail.com>
2018-03-23 15:41:47 +0700

    add reply

--- app.js
+++ app.js
@@ -31,20 +31,70 @@ function reply(reply_token, msg, user) {
         'Authorization': 'Bearer 7NOT+yYEOQQM2QMFxwG+4Jg+RAA0iKiqPFG/BlKXTHUug5+xjcrlg2uDayjzNZe0RrrmIpHct0XiSgZp4o2G8DM8B1I+Ih5gHdPd/tgd519YQc0B5+gnAHiP6D4ZNEJ0LLhMybZl++xkNDBRbLg6yQdB04t89/1O/w1cDnyilFU='
     }
     
-    let text = `รักนะ ${user.displayName} อึ้บๆ`
+    if (msg === 'build core') {
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: `เริ่มโปรเซส ${msg.toUpperCase()}`
+            }]
+        })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            request.post({
+                url: 'https://songshake:iL0veFungj%40i123@ci.songshakes.com/job/core/job/build/build?delay=0sec'
+            }, (err2, res2, body2) => {
+                
+                if (err2) {
+                    let body = JSON.stringify({
+                        replyToken: reply_token,
+                        messages: [{
+                            type: 'text',
+                            text: `โปรเซส ${msg.toUpperCase()} มีปัญหาลองทำรายการใหม่`
+                        }]
+                    })
+                }
+                else {
+                    let body = JSON.stringify({
+                        replyToken: reply_token,
+                        messages: [{
+                            type: 'text',
+                            text: `โปรเซส ${msg.toUpperCase()} สำเร็จจ้า`
+                        }]
+                    })
+                }
 
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: text
-        }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
+                request.post({
+                    url: 'https://api.line.me/v2/bot/message/reply',
+                    headers: headers,
+                    body: body
+                }, (err, res, body) => {
+                    
+                });
+            })
+        });
+        
+        
+    }
+    else {
+        let text = `รักนะ ${user.displayName} อึ้บๆ`
+
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                type: 'text',
+                text: text
+            }]
+        })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });
+    }
 }
